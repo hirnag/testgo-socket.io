72c4f820fc21d03c56d941319273969cca949c8c
hirokazu <t>
2018-03-16 06:42:32 +0900

    first commit

---
+++ multi_clients2node.js
@@ -0,0 +1,10 @@
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
+var NUM = (process.argv.length > 2)? process.argv[2] : 10;
+
+for(var i = 0; i<NUM; i++) {
+  var sp = spawn("node", ['client2node.js']);
+  sp.stdout.on("data", function(data){console.log("" + data)});
+}
+
